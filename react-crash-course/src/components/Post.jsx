79fd217697a1a532346cdc574bd42ee977bceb68
ygs <ygs3004@naver.com>
2024-12-02 03:09:52 +0900

    react - dynamic value

--- react-crash-course/src/components/Post.jsx
+++ react-crash-course/src/components/Post.jsx
@@ -1,6 +1,9 @@
+const names = ["Yoon", "Gun"];
+
 function Post() {
+    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
     return <div>
-        <p>Yoon</p>
+        <p>ChosenName: {chosenName}</p>
         <p>React.js is Awesome</p>
     </div>
 }
