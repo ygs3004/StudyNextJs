13d6f4ad7d787326cfb1e7843b406e33204b4791
ygs <ygs3004@naver.com>
2024-12-03 03:03:50 +0900

    react - css module

--- react-crash-course/src/components/Post.jsx
+++ react-crash-course/src/components/Post.jsx
@@ -1,8 +1,12 @@
+import classes from './Post.module.css';
+
 function Post(props) {
-    return <div>
-        <p>{props.author}</p>
-        <p>{props.body}</p>
-    </div>
+    return (
+        <div className={classes.post}>
+            <p className={classes.author}>{props.author}</p>
+            <p className={classes.text}>{props.body}</p>
+        </div>
+    )
 }
 
 export default Post;
