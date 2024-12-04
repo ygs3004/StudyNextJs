88593a958425aee1fa46e0dacc9203ac2c5674e1
ygs <ygs3004@naver.com>
2024-12-05 05:26:29 +0900

    react - buttons event

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -1,34 +1,19 @@
-import Post from "./Post.jsx";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost.jsx";
-import {useState} from "react";
 import Modal from "./Modal.jsx";
 
 function PostsList({isPosting, onStopPosting}) {
-    const [enteredBody, setEnteredBody] = useState("");
-    const [enteredAuth, setEnteredAuth] = useState("");
-
-    function bodyChangeHandler(event){
-        setEnteredBody(event.target.value);
-    }
-
-    function authChangeHandler(event){
-        setEnteredAuth(event.target.value);
-    }
-
     return (
         <>
             {isPosting && (
                 <Modal onClose={onStopPosting}>
                     <NewPost
-                        onBodyChange={bodyChangeHandler}
-                        onAuthorChange={authChangeHandler}
+                        onCancel={onStopPosting}
                     />
                 </Modal>
             )}
             <ul className={classes.posts}>
-                <Post author={enteredAuth} body={enteredBody}/>
-                <Post author="Gun Soo" body="Check out the full course!"/>
+                {/*<Post author={enteredAuth} body={enteredBody}/>*/}
             </ul>
         </>
     );
