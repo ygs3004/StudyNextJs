85fe9ae22f8f9a1a379bace0841e391675d6ffda
ygs <ygs3004@naver.com>
2024-12-16 02:59:51 +0900

    nextjs-foodie - 컴포넌트 추가

--- nextjs-starting/app/page.js
+++ nextjs-starting/app/page.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
-import {Header} from "@/components/header";
+import {MainHeader} from "@/components/main-header";
 
 export default function Home() {
     console.log("Server Console")
 
     return (
         <main>
-            <Header/>
             <p>🔥 Let&apos;s get started! 🔥</p>
-            <p><Link href="/about">About</Link></p>
+            <p><Link href="/meals">Meals</Link></p>
+            <p><Link href="/meals/share">Share Meal</Link></p>
+            <p><Link href="/community">Community</Link></p>
         </main>
     );
 }
