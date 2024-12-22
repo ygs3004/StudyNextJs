0c79e46ff8d2e4635dd443ab0deb3b3707eb46e9
ygs <ygs3004@naver.com>
2024-12-22 14:45:18 +0900

    nextjs-foodie  - image file

--- nextjs-foodie/components/meals/meals-grid.js
+++ nextjs-foodie/components/meals/meals-grid.js
@@ -3,10 +3,10 @@ import MealItem from "@/components/meals/meal-item";
 
 export default function MealsGrid({meals}) {
     return <ul className={classes.meals}>
-        {meals.map((meal) => {
+        {meals.map((meal) =>
             <li key={meal.id}>
                 <MealItem {...meal}/>
             </li>
-        })}
+        )}
     </ul>
 };
---
+++ nextjs-foodie/lib/meals.js
@@ -0,0 +1,9 @@
+import sql from 'better-sqlite3'
+
+const db = sql('meals.db');
+
+export async function getMeals(){
+    // async example 을 위한 코드
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return db.prepare('SELECT * FROM meals').all();
+}
