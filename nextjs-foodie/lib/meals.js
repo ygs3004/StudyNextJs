a5d7caf9f6c2b0668184e33a10d77c9bf8cc64a2
ygs <ygs3004@naver.com>
2024-12-22 15:59:00 +0900

    nextjs-foodie  - error.js

--- nextjs-foodie/lib/meals.js
+++ nextjs-foodie/lib/meals.js
@@ -5,5 +5,7 @@ const db = sql('meals.db');
 export async function getMeals(){
     // async example 을 위한 코드
     await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    // throw new Error('Loading meals failed');
     return db.prepare('SELECT * FROM meals').all();
 }
