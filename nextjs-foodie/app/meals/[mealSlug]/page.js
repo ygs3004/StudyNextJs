6497faa1457091020437e2bd2cf21fdcdf09d09a
ygs <ygs3004@naver.com>
2024-12-23 01:36:19 +0900

    nextjs-foodie  - not-found function

--- nextjs-foodie/app/meals/[mealSlug]/page.js
+++ nextjs-foodie/app/meals/[mealSlug]/page.js
@@ -1,9 +1,15 @@
 import classes from './page.module.css';
 import Image from "next/image";
 import {getMeal} from "@/lib/meals";
+import {notFound} from "next/navigation";
 
 export default function MealDetailsPage({params}) {
     const meal = getMeal(params.mealSlug);
+
+    if (!meal) {
+        notFound();
+    }
+
     meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
 
     return <>
