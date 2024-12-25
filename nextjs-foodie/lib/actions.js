396195c069bac6a67952a32518c0700515a62e9f
ygs <ygs3004@naver.com>
2024-12-25 23:04:02 +0900

    nextjs-foodie  - server validation

--- nextjs-foodie/lib/actions.js
+++ nextjs-foodie/lib/actions.js
@@ -3,6 +3,10 @@
 import {saveMeal} from "@/lib/meals";
 import {redirect} from "next/navigation";
 
+function isInvalidTest(text) {
+    return (!text || text.trim() === '');
+}
+
 export async function shareMeal(formData) {
     // input name 기반
     const meal = {
@@ -12,6 +16,18 @@ export async function shareMeal(formData) {
         image: formData.get("image"),
         creator: formData.get("name"),
         creator_email: formData.get("email"),
+    }
+
+    if (
+        isInvalidTest(meal.title) ||
+        isInvalidTest(meal.summary) ||
+        isInvalidTest(meal.instructions) ||
+        isInvalidTest(meal.creator) ||
+        isInvalidTest(meal.creator_email) ||
+        !meal.creator_email.includes('@') ||
+        !meal.image ||  meal.image.size === 0
+    ) {
+        throw new Error('Invalid Input Data');
     }
 
     await saveMeal(meal);
