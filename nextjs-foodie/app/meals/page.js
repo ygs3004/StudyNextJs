0110ae92ce55e3e4902c75b6eecfc6132174a617
ygs <ygs3004@naver.com>
2025-01-07 02:34:09 +0900

    nextjs-foodie  - 정적 / 동적 metadata

--- nextjs-foodie/app/meals/page.js
+++ nextjs-foodie/app/meals/page.js
@@ -4,11 +4,15 @@ import MealsGrid from "@/components/meals/meals-grid";
 import {getMeals} from "@/lib/meals";
 import {Suspense} from "react";
 
+export const metadata = {
+    title: 'All Meals',
+    description: 'Browse the delicious meals shared by our vibrant community.',
+};
+
 async function Meals() {
     const meals = await getMeals();
     return <MealsGrid meals={meals}/>
 }
-
 
 export default async function MealsPage() {
 
