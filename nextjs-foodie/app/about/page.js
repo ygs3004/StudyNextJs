827c27f493aa0223467a54898a06be47a77356c3
ygs <ygs3004@naver.com>
2025-01-10 02:28:46 +0900

    rename

--- nextjs-foodie/app/about/page.js
+++
@@ -1,5 +0,0 @@
-export default function AbountPage() {
-    return <>
-        <h1>About1</h1>
-    </>
-};
--- nextjs-foodie/app/meals/error.js
+++
@@ -1,8 +0,0 @@
-'use client'
-
-export default function Error({error}) {
-    return <main className="error">
-        <h1>An error occurred!</h1>
-        <p>Failed to fetch meal data. Please try again later</p>
-    </main>
-}
