254dc48dc82bfac25186770fbe50d45264c4f63f
ygs <ygs3004@naver.com>
2026-01-09 22:38:29 +0900

    nextjs - routing & page rendering - not found page

---
+++ nextjs-routing/app/news/[slug]/not-found.js
@@ -0,0 +1,6 @@
+export default function NewsNotFoundPage() {
+    return <div id="error">
+        <h1>Not Found</h1>
+        <p>Unfortunately, we could not find the request article.</p>
+    </div>
+};
---
+++ nextjs-routing/app/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFoundPage() {
+    return <div id="error">
+        <h1>Not Found</h1>
+        <p>The request resource could not be found!</p>
+    </div>
+};
