3ca6ca006264fb0100c68e730c5469474e8314c0
ygs <ygs3004@naver.com>
2026-01-12 22:03:51 +0900

    nextjs - routing & page rendering - Catch-All 폴백 라우트 & 추가 경로

--- nextjs-routing/app/archive/@archive/[[...filter]]/page.js
+++ nextjs-routing/app/archive/@archive/[[...filter]]/page.js
@@ -1,19 +1,50 @@
-import {getAvailableNewsYears, getNewsForYear} from "@/app/lib/news";
+import {getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth} from "@/app/lib/news";
 import Link from "next/link";
+import NewsList from "@/public/component/news-list";
 
 export default function FilteredNewsPage({params}) {
-    const filter = params.filter
+    const filter = params.filter;
 
-    const links = getAvailableNewsYears();
-    return <header id="archive-header">
-        <nav>
-            <ul>
-                {links.map(link =>
-                    <li key={link}>
-                        <Link href={`/archive/${link}`}>{link}</Link>
-                    </li>
-                )}
-            </ul>
-        </nav>
-    </header>
+    const selectedYear = filter?.[0];
+    const selectedMonth = filter?.[1];
+
+    let news;
+    let links = getAvailableNewsYears();
+
+    if(selectedYear && !selectedMonth) {
+        news = getNewsForYear(selectedYear);
+        links = getAvailableNewsMonths(selectedYear)
+    }
+
+    if(selectedMonth && selectedMonth) {
+        news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+        links = getAvailableNewsMonths(selectedYear, selectedMonth)
+    }
+
+    let newsContent = <p>No news found for the selected period.</p>
+
+    if(news && news.length > 0) {
+        newsContent = <NewsList news={news}/>;
+    }
+
+    return (
+        <>
+            <header id="archive-header">
+                <nav>
+                    <ul>
+                        {links.map((link) => {
+                            const href = selectedYear ? `/archive/${selectedYear}/${link}` : `/archive/${link}`;
+                            return (
+                                    <li key={link}>
+                                        <Link href={href}>{link}</Link>
+                                    </li>
+                                )
+                        })}
+                    </ul>
+                </nav>
+            </header>
+            {newsContent}
+        </>
+    )
+
 };
