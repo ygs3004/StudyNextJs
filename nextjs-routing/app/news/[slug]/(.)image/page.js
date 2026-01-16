c9af41a2aa9031e657125c3fecea40347f89b330
ygs <ygs3004@naver.com>
2026-01-16 18:53:36 +0900

    nextjs - routing & page rendering - Intercepting route

--- nextjs-routing/app/news/[slug]/(.)image/page.js
+++ nextjs-routing/app/news/[slug]/(.)image/page.js
@@ -1,7 +1,7 @@
 import {DUMMY_NEWS} from "@/dummy-news";
 import {notFound} from "next/navigation";
 
-export default function ImagePage({params}) {
+export default function InterceptedImagePage({params}) {
     const newsItemSlug = params.slug
     const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsItemSlug)
 
@@ -10,8 +10,11 @@ export default function ImagePage({params}) {
     }
 
     return (
-        <div className="fullscreen-image">
-            <img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/>
-        </div>
+        <>
+            <h2>Intercepted</h2>
+            <div className="fullscreen-image">
+                <img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/>
+            </div>
+        </>
     )
 };
