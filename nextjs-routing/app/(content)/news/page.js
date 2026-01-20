4bbd850e8886575d035632b1f46d4ad1d2a2a26f
ygs <ygs3004@naver.com>
2026-01-20 23:20:05 +0900

    nextjs - data fetching - 데이터 가져오기 - 클라이언트

--- nextjs-routing/app/(content)/news/page.js
+++ nextjs-routing/app/(content)/news/page.js
@@ -1,12 +1,48 @@
-import Link from "next/link";
-import {DUMMY_NEWS} from "@/dummy-news";
-import NewsList from "@/public/component/news-list";
+"use client"
 
-export default function News() {
+import NewsList from "@/public/component/news-list";
+import {useEffect, useState} from "react";
+
+export default function NewsPage() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const [news, setNews] = useState();
+
+    useEffect(() => {
+        async function fetchNews() {
+            setIsLoading(true)
+            const response = await fetch('http://localhost:8080/news');
+
+            if (!response.ok) {
+                setError('Failed to fetch news')
+                setIsLoading(false);
+            }
+
+            const news = await response.json();
+            setIsLoading(false);
+            setNews(news)
+        }
+
+       fetchNews();
+    }, []);
+
+    if (isLoading) {
+        return <p>Loading....</p>
+    }
+
+    if (error) {
+        return <p>{error}</p>
+    }
+
+    let newsContent;
+    if (news) {
+        newsContent = <NewsList news={news}/>
+    }
+
     return (
         <>
             <h1>News Page</h1>
-            <NewsList news={DUMMY_NEWS}/>
+            {newsContent}
         </>
     );
 }
