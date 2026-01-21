13e9641c8bad937e6b10c0d79de5e0d329ba9100
ygs <ygs3004@naver.com>
2026-01-21 18:01:37 +0900

    nextjs - data fetching - 데이터 가져오기 - 백엔드 기반(Next.js)

--- nextjs-routing/app/(content)/news/page.js
+++ nextjs-routing/app/(content)/news/page.js
@@ -1,48 +1,19 @@
-"use client"
-
 import NewsList from "@/public/component/news-list";
-import {useEffect, useState} from "react";
 
-export default function NewsPage() {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState();
-    const [news, setNews] = useState();
+export default async function NewsPage() {
 
-    useEffect(() => {
-        async function fetchNews() {
-            setIsLoading(true)
-            const response = await fetch('http://localhost:8080/news');
+    const response = await fetch('http://localhost:8080/news');
 
-            if (!response.ok) {
-                setError('Failed to fetch news')
-                setIsLoading(false);
-            }
-
-            const news = await response.json();
-            setIsLoading(false);
-            setNews(news)
-        }
-
-       fetchNews();
-    }, []);
-
-    if (isLoading) {
-        return <p>Loading....</p>
+    if(!response.ok){
+        throw new Error("Failed to fetch news.");
     }
 
-    if (error) {
-        return <p>{error}</p>
-    }
-
-    let newsContent;
-    if (news) {
-        newsContent = <NewsList news={news}/>
-    }
+    const news = await response.json();
 
     return (
         <>
             <h1>News Page</h1>
-            {newsContent}
+            <NewsList news={news}/>
         </>
     );
 }
