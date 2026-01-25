13cc24c174f490d5841cb86a9467ac3b0b71386e
ygs <ygs3004@naver.com>
2026-01-25 22:52:12 +0900

    nextjs - data fetching - Suspense 이용하여 동일 컴포넌트에서 로딩 세분화

--- nextjs-routing/app/(content)/archive/@archive/[[...filter]]/page.js
+++ nextjs-routing/app/(content)/archive/@archive/[[...filter]]/page.js
@@ -1,24 +1,52 @@
 import {getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth} from "@/app/lib/news";
 import Link from "next/link";
 import NewsList from "@/component/news-list";
+import {Suspense} from "react";
 
-export default async function FilteredNewsPage({params}) {
-    const filter = params.filter;
+async function FilteredHeader({year, month}) {
+    const availableYears = await getAvailableNewsYears();
+    let links = availableYears;
 
-    const selectedYear = filter?.[0];
-    const selectedMonth = filter?.[1];
-
-    let news;
-    let links = await getAvailableNewsYears();
-
-    if(selectedYear && !selectedMonth) {
-        news = await getNewsForYear(selectedYear);
-        links = getAvailableNewsMonths(selectedYear)
+    if(
+        (year && !availableYears.includes(year)) ||
+        (month && !getAvailableNewsMonths(year).includes(month))
+    ){
+        throw new Error('Invalid Filter.')
     }
 
-    if(selectedMonth && selectedMonth) {
-        news = await getNewsForYearAndMonth(selectedYear, selectedMonth);
-        links = getAvailableNewsMonths(selectedYear, selectedMonth)
+    if(year && !month) {
+        links = getAvailableNewsMonths(year)
+    }
+
+    if(year && month) {
+        links = [];
+    }
+
+    return (
+        <header id="archive-header">
+            <nav>
+                <ul>
+                    {links.map((link) => {
+                        const href = year ? `/archive/${year}/${link}` : `/archive/${link}`;
+                        return (
+                            <li key={link}>
+                                <Link href={href}>{link}</Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+async function FilteredNews({year, month}){
+    let news;
+
+    if(year && !month){
+        news = await getNewsForYear(year);
+    }else if (year && month) {
+        news = await getNewsForYearAndMonth(year, month)
     }
 
     let newsContent = <p>No news found for the selected period.</p>
@@ -27,32 +55,25 @@ export default async function FilteredNewsPage({params}) {
         newsContent = <NewsList news={news}/>;
     }
 
-    const availableYears = await getAvailableNewsYears();
+    return newsContent;
+}
 
-    if(
-        (selectedYear && !availableYears.includes(selectedYear)) ||
-        (selectedMonth && !getAvailableNewsMonths(selectedYear).includes(selectedMonth))
-    ){
-        throw new Error('Invalid Filter.')
-    }
+export default async function FilteredNewsPage({params}) {
+    const filter = params.filter;
+
+    const selectedYear = filter?.[0];
+    const selectedMonth = filter?.[1];
 
     return (
         <>
-            <header id="archive-header">
-                <nav>
-                    <ul>
-                        {links.map((link) => {
-                            const href = selectedYear ? `/archive/${selectedYear}/${link}` : `/archive/${link}`;
-                            return (
-                                    <li key={link}>
-                                        <Link href={href}>{link}</Link>
-                                    </li>
-                                )
-                        })}
-                    </ul>
-                </nav>
-            </header>
-            {newsContent}
+            {/*<Suspense fallback={<p>Loading Filter...</p>}>*/}
+            {/*    <FilteredHeader year={selectedYear} month={selectedMonth}/>*/}
+            {/*</Suspense>*/}
+
+            <Suspense fallback={<p>Loading News...</p>}>
+                <FilteredHeader year={selectedYear} month={selectedMonth}/>
+                <FilteredNews year={selectedYear} month={selectedMonth}/>
+            </Suspense>
         </>
     )
 
