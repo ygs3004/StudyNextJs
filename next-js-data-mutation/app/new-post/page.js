caee8e13b6e11b901ce2e84fda123d7f8f775e23
ygs <ygs3004@naver.com>
2026-01-29 21:32:18 +0900

    nextjs - data mutation - useActionState(useFormState) 유효성 체크

--- next-js-data-mutation/app/new-post/page.js
+++ next-js-data-mutation/app/new-post/page.js
@@ -1,50 +1,45 @@
 import {storePost} from "@/lib/posts";
 import {redirect} from "next/navigation";
-import FormSubmit from "@/components/form-submit";
+import PostForm from "@/components/post-form";
 
 export default function NewPostPage() {
-    async function createPost(formData) {
+
+    async function createPost(prevState, formData) {
         "use server";
 
         const title = formData.get("title");
         const image = formData.get("image");
         const content = formData.get("content");
 
+        let errors = [];
+
+        if (!title || title.trim().length === 0) {
+            errors.push("제목은 필수입니다.")
+        }
+
+        if (!content || content.trim().length === 0) {
+            errors.push("내용은 필수입니다.")
+        }
+
+        if (!image || image.size === 0) {
+            errors.push("이미지는 필수입니다.")
+        }
+
+        if (errors.length > 0) {
+            return {
+                errors
+            }
+        }
+
         await storePost({
             imageUrl: "",
             title,
             content,
             userId: 1,
-        })
+        });
 
         redirect("/feed");
     }
 
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit/>
-        </p>
-      </form>
-    </>
-  );
+    return <PostForm action={createPost} />
 }
--- next-js-data-mutation/components/form-submit.js
+++ next-js-data-mutation/components/form-submit.js
@@ -1,10 +1,11 @@
 "use client"
 
-import {useFormStatus} from 'react-dom';
+import {useFormStatus} from "react-dom";
 
 export default function FormSubmit() {
 
     const status = useFormStatus();
+
     if (status.pending) {
         return <p>Creating post...</p>
     }
