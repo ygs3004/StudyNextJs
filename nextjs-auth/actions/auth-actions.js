476b8a79ee9cca4182c308feeecebe76a09cc7a8
ygs <ygs3004@naver.com>
2026-02-21 23:30:48 +0900

    nextjs - auth - signup validation

---
+++ nextjs-auth/actions/auth-actions.js
@@ -0,0 +1,25 @@
+"use server";
+
+export async function signup(prevState, formData) {
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    let errors = {};
+
+    if (!email.includes("@")) {
+        errors.email = "이메일 형식이 아닙니다."
+    }
+
+    if (password.trim().length < 8) {
+        errors.password = "비밀번호는 8자 이상 이어야 합니다."
+    }
+
+    if (Object.keys(errors).length > 0) {
+        return {
+            errors
+        }
+    }
+
+    // create a new user
+
+}
