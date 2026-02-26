955403de121541df22d8401b1b426a364f97bb5e
ygs <ygs3004@naver.com>
2026-02-27 00:22:39 +0900

    nextjs - auth - verify session

--- nextjs-auth/lib/auth.js
+++ nextjs-auth/lib/auth.js
@@ -26,3 +26,47 @@ export async function createAuthSession(userId) {
         sessionCookie.attributes
     );
 }
+
+export async function verifyAuth(userId) {
+    const sessionCookie =  cookies().get(lucia.sessionCookieName);
+
+    if (!sessionCookie) {
+        return {
+            user: null,
+            session: null
+        }
+    }
+
+    const sessionId = sessionCookie.value;
+    if(!sessionId){
+        return {
+            user: null,
+            session: null
+        }
+    }
+
+    const result = await lucia.validateSession(sessionId);
+
+    try {
+        if (result.session && result.session.fresh) {
+            const sessionCookie = lucia.createSessionCookie(result.session.id);
+            cookies().set(
+                sessionCookie.name,
+                sessionCookie.value,
+                sessionCookie.attributes
+            );
+        }
+
+        if (!result.session) {
+            const sessionCookie = lucia.createBlankSessionCookie();
+            cookies.set(
+                sessionCookie.name,
+                sessionCookie.value,
+                sessionCookie.attributes
+            )
+        }
+    } catch (error){
+    }
+
+    return result;
+}
