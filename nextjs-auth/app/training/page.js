7718cb59a830f59658d5180905bf2f69265091f5
ygs <ygs3004@naver.com>
2026-02-28 20:38:35 +0900

    nextjs - auth - login & signup mode 처리

--- nextjs-auth/app/training/page.js
+++ nextjs-auth/app/training/page.js
@@ -1,22 +1,30 @@
-import { getTrainings } from '@/lib/training';
+import {getTrainings} from '@/lib/training';
+import {verifyAuth} from "@/lib/auth";
+import {redirect} from "next/navigation";
 
 export default async function TrainingPage() {
-  const trainingSessions = getTrainings();
+    const result = await verifyAuth();
 
-  return (
-    <main>
-      <h1>Find your favorite activity</h1>
-      <ul id="training-sessions">
-        {trainingSessions.map((training) => (
-          <li key={training.id}>
-            <img src={`/trainings/${training.image}`} alt={training.title} />
-            <div>
-              <h2>{training.title}</h2>
-              <p>{training.description}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
+    if (!result.user) {
+        return redirect("/");
+    }
+
+    const trainingSessions = getTrainings();
+
+    return (
+        <main>
+            <h1>Find your favorite activity</h1>
+            <ul id="training-sessions">
+                {trainingSessions.map((training) => (
+                    <li key={training.id}>
+                        <img src={`/trainings/${training.image}`} alt={training.title}/>
+                        <div>
+                            <h2>{training.title}</h2>
+                            <p>{training.description}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
 }
--- nextjs-auth/components/auth-form.js
+++ nextjs-auth/components/auth-form.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import {useFormState} from "react-dom";
 import {signup} from "@/actions/auth-actions";
 
-export default function AuthForm() {
+export default function AuthForm({mode}) {
 
     const [formState, formAction] = useFormState(signup, {});
 
@@ -30,11 +30,16 @@ export default function AuthForm() {
             )}
             <p>
                 <button type="submit">
-                    Create Account
+                    {mode === "login" ? "Login" : "Create Account"}
                 </button>
             </p>
             <p>
-                <Link href="/">Login with existing account.</Link>
+                {mode === "login" && (
+                    <Link href="/?mode=signup">Create an account.</Link>
+                )}
+                {mode === "signup" && (
+                    <Link href="/?mode=login">Login with existing account.</Link>
+                )}
             </p>
         </form>
     );
