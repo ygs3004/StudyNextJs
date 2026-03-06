fec96789e4fe9aef46e537f2316a6ca80a05f837
ygs <ygs3004@naver.com>
2026-03-06 23:47:08 +0900

    nextjs - page routing - dynamic route segment

--- nextjs-page-routing/pages/portfolio/[projectid].js
+++ nextjs-page-routing/pages/portfolio/[projectid].js
@@ -1,4 +1,12 @@
+import {useRouter} from "next/router";
+
 function PortfolioProjectPage() {
+    const router = useRouter();
+
+    console.log(router.pathname);
+    console.log(router.query);
+    // router.query.projectid 를 통하여 동적 경로에 접근 가능
+
     return (
         <div>
             <h1>Portfolio Project Page</h1>
