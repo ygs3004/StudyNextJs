1d472c4ccfd2cdd162664ab714159789bfb2efc1
ygs <ygs3004@naver.com>
2026-03-08 22:57:40 +0900

    nextjs - page routing - Link, dynamic list

--- nextjs-page-routing/pages/clients/index.js
+++ nextjs-page-routing/pages/clients/index.js
@@ -1,6 +1,29 @@
+import Link from "next/link";
+
 function ClientsPage() {
+
+    const clients = [
+        {
+            id: "ygs",
+            name: "YoonGunSoo",
+        },
+        {
+            id: "lsh",
+            name: "LeeSangHyuk"
+        }
+    ]
+
     return <div>
         <h1>The Clients Page</h1>
+        <ul>
+            {
+                clients.map((client) =>
+                    <li key={client.id}>
+                        <Link href={`/clients/${client.id}`} >{client.name}</Link>
+                    </li>
+                )
+            }
+        </ul>
     </div>
 }
 
