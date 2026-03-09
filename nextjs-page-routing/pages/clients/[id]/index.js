c48ac99455abd142ff25f99dddde5bdb14c2bdb3
ygs <ygs3004@naver.com>
2026-03-09 23:50:46 +0900

    nextjs - page routing - navigating on click

--- nextjs-page-routing/pages/clients/[id]/index.js
+++ nextjs-page-routing/pages/clients/[id]/index.js
@@ -1,13 +1,26 @@
 import {useRouter} from "next/router";
+import clientprojectid from "./[clientprojectid]";
 
 function ClientProjectsPage() {
     const router = useRouter();
 
-    console.log(router.query);
+    // console.log(router.query);
+
+    function loadProjectHandler() {
+        // router.push("/clients/max/project");
+        router.push({
+            pathname: "/clients/[id]/[clientprojectid]",
+            query: {
+                id: "max",
+                clientprojectid: "project",
+            }
+        })
+    }
 
     return (
         <div>
             <h1>The Projects of a Given Client</h1>
+            <button onClick={loadProjectHandler}>Load Project</button>
         </div>
     )
 }
