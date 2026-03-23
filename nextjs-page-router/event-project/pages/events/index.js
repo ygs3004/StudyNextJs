cc2262e47e409d496c426e3762f5d0277fd4ad0a
ygs <ygs3004@naver.com>
2026-03-24 02:44:46 +0900

    nextjs - page route project - All Event, Filtering Page Style

--- nextjs-page-router/event-project/pages/events/index.js
+++ nextjs-page-router/event-project/pages/events/index.js
@@ -1,8 +1,16 @@
+import {getAllEvents} from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/events-search";
+import {Fragment} from "react";
+
 function AllEventPage() {
+    const events = getAllEvents();
+
     return (
-        <div>
-            <h1>All Event</h1>
-        </div>
+        <Fragment>
+            <EventSearch />
+            <EventList items={events}/>
+        </Fragment>
     )
 }
 
