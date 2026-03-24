9f438bac4a162d048163c1917e6f066f2fc7e8f8
ygs <ygs3004@naver.com>
2026-03-25 01:47:00 +0900

    nextjs - page route project - Filter Event Navigating

--- nextjs-page-router/event-project/pages/events/index.js
+++ nextjs-page-router/event-project/pages/events/index.js
@@ -2,13 +2,21 @@ import {getAllEvents} from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventSearch from "../../components/events/events-search";
 import {Fragment} from "react";
+import {useRouter} from "next/router";
 
 function AllEventPage() {
+    const router = useRouter();
     const events = getAllEvents();
+
+    function findEventsHandler(year, month) {
+        const fullPath = `/events/${year}/${month}`;
+
+        router.push(fullPath);
+    }
 
     return (
         <Fragment>
-            <EventSearch />
+            <EventSearch onSearch={findEventsHandler}/>
             <EventList items={events}/>
         </Fragment>
     )
