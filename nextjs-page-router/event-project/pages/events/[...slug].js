8bd5b9b4cc57b738eff959561d7e0caff0f21ca7
ygs <ygs3004@naver.com>
2026-03-26 23:23:59 +0900

    nextjs - page route project - Filter Event Page Data

--- nextjs-page-router/event-project/pages/events/[...slug].js
+++ nextjs-page-router/event-project/pages/events/[...slug].js
@@ -1,7 +1,40 @@
+import {useRouter} from "next/router";
+import {getFilteredEvents} from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+
 function FilteredEventPage() {
+    const router = useRouter();
+    const filteredData = router.query.slug;
+
+    if (!filteredData) {
+        return (
+            <p className="center">Loading...</p>
+        )
+    }
+
+    const filteredYear = filteredData[0];
+    const filteredMonth = filteredData[1];
+    const numYear = +filteredYear;
+    const numMonth = +filteredMonth;
+
+    if (isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
+        return (
+            <p>올바르지 않은 검색 조건입니다.</p>
+        )
+    }
+
+    const filteredEvents = getFilteredEvents({
+        year: numYear,
+        month: numMonth,
+    })
+
+    if(!filteredEvents || filteredEvents.length === 0) {
+        return <p>No Event Found for the chosen filter</p>
+    }
+
     return (
         <div>
-            <h1>Filtered Events</h1>
+            <EventList items = {filteredEvents} />
         </div>
     )
 }
