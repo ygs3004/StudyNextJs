6c598f8d931606490960950ae30230cf860572f2
ygs <ygs3004@naver.com>
2026-03-31 00:40:02 +0900

    nextjs - page route, rendering & fetching - static generation, getStaticProps - revalidate, 증분 정적 생성(ISR)

--- nextjs-page-router/rendering-and-fetching/pages/index.js
+++ nextjs-page-router/rendering-and-fetching/pages/index.js
@@ -15,6 +15,7 @@ function HomePage(props) {
 
 // 서버 사이드 static generation, 클라이언트로 전달되지 않는 코드
 export async function getStaticProps() {
+    console.log("Re-generating.........................");
     const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
     const jsonData = await fs.readFile(filePath);
     const data = JSON.parse(jsonData);
@@ -22,7 +23,8 @@ export async function getStaticProps() {
     return {
         props: {
             products: data.products,
-        }
+        },
+        revalidate: 10, // 증분 정적 생성(ISR), 시간마다 페이지를 생성해둠, 개발 중에는 매번 재생성, production 에선 설정한 시간 단위로 재생성
     }
 }
 
