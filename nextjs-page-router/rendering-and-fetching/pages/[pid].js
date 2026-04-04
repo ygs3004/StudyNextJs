8401b1988f5e05358e7fd0722129c9936708172d
ygs <ygs3004@naver.com>
2026-04-04 23:07:10 +0900

    nextjs - page route, rendering & fetching - dynamic path loading case

--- nextjs-page-router/rendering-and-fetching/pages/[pid].js
+++ nextjs-page-router/rendering-and-fetching/pages/[pid].js
@@ -6,6 +6,10 @@ function ProductDetailPage(props) {
 
     const {loadedProduct} = props;
 
+    if(!loadedProduct){
+        return <p>Loading....</p>
+    }
+
     return (
         <Fragment>
             <h1>{loadedProduct.title}</h1>
@@ -18,27 +22,40 @@ export async function getStaticProps(context) {
     const {params} = context;
     const productId = params.pid;
 
-    const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-    const jsonData = await fs.readFile(filePath);
-    const data = JSON.parse(jsonData);
+    const data = getData();
 
     const product = data.products.find(product => product.id === productId);
+
+    if (!product) {
+        return {
+            notFound: true,
+        }
+    }
 
     return {
         props: {
             loadedProduct: product,
         }
-    }
+    };
+}
+
+async function getData() {
+    const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    return data
 }
 
 export async function getStaticPaths() {
+    const data = await getData();
+
+    const ids = data.products.map(product => product.id);
+    const pathWithParams = ids.map(id => ({params: {pid: id}}))
+
     return {
-        paths: [
-            {params: {pid: "p1"}},
-            {params: {pid: "p2"}},
-            {params: {pid: "p3"}},
-        ],
-        fallback: false,
+        paths: pathWithParams,
+        fallback: true,
     }
 }
 
