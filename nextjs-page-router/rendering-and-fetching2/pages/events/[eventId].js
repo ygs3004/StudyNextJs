08c690fd4792e3efd8dd432e459f3a4387adba6a
ygs <ygs3004@naver.com>
2026-05-26 21:59:16 +0900

    nextjs - page route, rendering & fetching second project - 동적페이지 데이터 & 경로 처리

--- nextjs-page-router/rendering-and-fetching2/pages/events/[eventId].js
+++ nextjs-page-router/rendering-and-fetching2/pages/events/[eventId].js
@@ -1,17 +1,13 @@
 import { Fragment } from 'react';
-import { useRouter } from 'next/router';
 
-import { getEventById } from '../../dummy-data';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
+import {getEventById, getAllEvents} from "../../helpers/api-utils";
 
-function EventDetailPage() {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
@@ -35,6 +31,35 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+    const eventId = context.params.eventId;
+
+    const event = await getEventById(eventId);
+
+    return {
+        props: {
+            selectedEvent: event,
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    const events = await getAllEvents();
+
+    const paths = events.map(event => (
+        {
+            params: {
+                eventId: event.id
+            }
+        }
+    ));
+
+    return {
+        paths: paths,
+        fallback: false // 지정된 paths 외의 경로 fallback 버젼 여부, false 시 notFound 페이지
+    }
 }
 
 export default EventDetailPage;
